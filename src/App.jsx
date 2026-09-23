1f2844ac95165023e1e165b2d8aae1813782223d
Borripat Namchang <borripat25471@gmail.com>
2026-09-23 07:53:20 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -42,26 +42,7 @@ export default function App() {
       figmaUrl: ""
       
     },
-    {
-      title: "ดดดดดด",
-      category: "UX/UI Design",
-      description: "เว็บไซต์นำเสนอเทคโนโลยีโฮโลแกรม ออกแบบ Information Architecture และ Responsive Layout ให้นำเสนอข้อมูลที่ซับซ้อนให้อ่านง่าย พร้อมจัดทำ Interactive Prototype",
-      tags: ["Figma", "UX Research", "Information Architecture", "Responsive Design"],
-      linkText: "Figma Prototype",
-      linkUrl: "https://www.figma.com/design/sK0dyZHRRHM9NJ8p8ulj0Z/HoloVista?node-id=0-1&t=HysPQqYXwKfneXuE-1",
-      figmaUrl: ""
-      
-    },
-    {
-      title: "ดดดดดด",
-      category: "UX/UI Design",
-      description: "เว็บไซต์นำเสนอเทคโนโลยีโฮโลแกรม ออกแบบ Information Architecture และ Responsive Layout ให้นำเสนอข้อมูลที่ซับซ้อนให้อ่านง่าย พร้อมจัดทำ Interactive Prototype",
-      tags: ["Figma", "UX Research", "Information Architecture", "Responsive Design"],
-      linkText: "Figma Prototype",
-      linkUrl: "https://www.figma.com/design/sK0dyZHRRHM9NJ8p8ulj0Z/HoloVista?node-id=0-1&t=HysPQqYXwKfneXuE-1",
-      figmaUrl: ""
-      
-    },
+   
   ];
 
   const filteredProjects = projectsData.filter(project => 
